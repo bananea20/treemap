42cace28d6262afd038ee6e0df7854a92725b0df
LEGION <>
2023-04-22 20:19:14 +0800

    无

--- function.py
+++ function.py
@@ -1,14 +1,4 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import squarify_modify  as  squarify  
 
-
-def draw_treemap(weight_list, label_list):
-
-    df = pd.DataFrame({'weight':weight_list, 'group':label_list })
-
-    # plot it
-    
-    squarify.plot(sizes=df['weight'], label=df['group'], alpha=.8,rectangle_size=rectangle_size,pad=Gap_size,value=value)
-    plt.axis('off')
-    plt.show()
-    
