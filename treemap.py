66ab18e39de37676b51e0c3769f2777d54f45756
LEGION <>
2023-04-22 20:33:42 +0800

    运行treemap.py，生成treemap.png

--- treemap.py
+++ treemap.py
@@ -1,11 +1,32 @@
-import matplotlib.pyplot as plt
-import squarify    # pip install squarify (algorithm for treemap)
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import squarify_modify  as  squarify  
+from function import *
 # Create a data frame with fake data
-df = pd.DataFrame({'nb_people':[8,3,4,2], 'group':["group A", "group B", "group C", "group D"] })
 
-# plot it
-squarify.plot(sizes=df['nb_people'], label=df['group'], alpha=.8 )
-plt.axis('off')
-plt.show()
+
+
+weight_list = [8,3,4,2]
+label_list = ["group A", "group B", "group C", "group D"]
+value = [1,2,3,4]
+
+
+
+# 间隙像素,如果为0则没有间隙
+Gap_size=2
+# 圆角半径，如果为0则没有圆角
+rectangle_size = 5
+
+def draw_treemap(weight_list, label_list):
+
+    df = pd.DataFrame({'weight':weight_list, 'group':label_list })
+
+    # plot it
+    
+    squarify.plot(sizes=df['weight'], label=df['group'], alpha=.8,rectangle_size=rectangle_size,pad=Gap_size,value=value)
+    plt.axis('off')
+    plt.show()
+    
+
+    
+draw_treemap(weight_list, label_list)
