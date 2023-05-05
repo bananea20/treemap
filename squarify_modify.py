f9e61bea6dcd3a0c4b1fdbce65aa42ecf80b1646
LEGION <>
2023-05-05 09:10:26 +0800

    加注释

--- squarify_modify.py
+++ squarify_modify.py
@@ -3,25 +3,10 @@
 #   (but not using their pseudocode)
 
 
-# INTERNAL FUNCTIONS not meant to be used by the user
 
-# 函数的目的是在一个矩形周围添加一个像素宽度的边框，使矩形更加突出
-# def pad_rectangle(rect):
-#     if rect["dx"] > 2:
-#         rect["x"] += 20
-#         rect["dx"] -= 0
-#     if rect["dy"] > 2:
-#         rect["y"] += 20
-#         rect["dy"] -= 0
-        
-# def pad_rectangle(rect):
-#     if rect["dx"] > 2:
-#         rect["x"] += 1
-#         rect["dx"] -= 2
-#     if rect["dy"] > 2:
-#         rect["y"] += 1
-#         rect["dy"] -= 2
 
+
+# 缩小矩形，左上角坐标增加n，宽高减少2n
 def pad_rectangle(rect, n):
     rect["x"] += n
     rect["dx"] -= 2*n
@@ -31,7 +16,7 @@ def pad_rectangle(rect, n):
 
 
 # 
-def layoutrow(sizes, x, y, dx, dy):
+def layoutrow(sizes, x, y, dx, dy): # 高度dx > 宽度dy
     # generate rects for each size in sizes
     # dx >= dy
     # they will fill up height dy, and width will be determined by their area
@@ -49,6 +34,10 @@ def layoutrow(sizes, x, y, dx, dy):
         # dx代表该矩形的宽度，而dy代表该矩形的高度。
 
     #最后，函数返回矩形列表rects。
+    '''
+       输入是每个矩形的面积以及大框的左上坐标和宽高
+       输出是每个子矩形的左上坐标和宽高   
+    '''
     covered_area = sum(sizes)
     width = covered_area / dy
     rects = []
@@ -56,6 +45,8 @@ def layoutrow(sizes, x, y, dx, dy):
         rects.append({"x": x, "y": y, "dx": width, "dy": size / width})
         y += size / width
     return rects
+
+
 
 
 def layoutcol(sizes, x, y, dx, dy):
@@ -81,26 +72,31 @@ def layout(sizes, x, y, dx, dy):
 
 
 def leftoverrow(sizes, x, y, dx, dy):
-    # compute remaining area when dx >= dy
-    covered_area = sum(sizes)
-    width = covered_area / dy
-    leftover_x = x + width
-    leftover_y = y
-    leftover_dx = dx - width
-    leftover_dy = dy
+    # 计算当 dx >= dy 时剩余区域的位置和尺寸
+
+    covered_area = sum(sizes)  # 计算已覆盖区域的总面积
+    width = covered_area / dy  # 计算已覆盖区域的宽度
+    leftover_x = x + width  # 计算剩余区域的 x 坐标
+    leftover_y = y  # 剩余区域的 y 坐标与输入 y 相同
+    leftover_dx = dx - width  # 计算剩余区域的宽度
+    leftover_dy = dy  # 剩余区域的高度与输入 dy 相同
     return (leftover_x, leftover_y, leftover_dx, leftover_dy)
+
 # 最终，函数返回一个包含剩余空间左上角坐标和宽度高度的元组
 #   (leftover_x, leftover_y, leftover_dx, leftover_dy)
 
 def leftovercol(sizes, x, y, dx, dy):
-    # compute remaining area when dx >= dy
-    covered_area = sum(sizes)
-    height = covered_area / dx
-    leftover_x = x
-    leftover_y = y + height
-    leftover_dx = dx
-    leftover_dy = dy - height
+    # 计算当 dx < dy 时剩余区域的位置和尺寸
+
+    covered_area = sum(sizes)  # 计算已覆盖区域的总面积
+    height = covered_area / dx  # 计算已覆盖区域的高度
+    leftover_x = x  # 剩余区域的 x 坐标与输入 x 相同
+    leftover_y = y + height  # 计算剩余区域的 y 坐标
+    leftover_dx = dx  # 剩余区域的宽度与输入 dx 相同
+    leftover_dy = dy - height  # 计算剩余区域的高度
     return (leftover_x, leftover_y, leftover_dx, leftover_dy)
+
+
 
 
 def leftover(sizes, x, y, dx, dy):
@@ -112,12 +108,14 @@ def leftover(sizes, x, y, dx, dy):
 
 
 def worst_ratio(sizes, x, y, dx, dy):
+    # 计算给定 sizes、位置和尺寸下，矩形树图布局中矩形的最大长宽比
     return max(
         [
             max(rect["dx"] / rect["dy"], rect["dy"] / rect["dx"])
             for rect in layout(sizes, x, y, dx, dy)
         ]
     )
+
 
 
 # PUBLIC API
@@ -147,27 +145,54 @@ def squarify(sizes, x, y, dx, dy):
         Each dict in the returned list represents a single rectangle in the
         treemap. The order corresponds to the input order.
     """
-    sizes = list(map(float, sizes))
+    """计算矩形树图。
 
-    if len(sizes) == 0:
+    给定一组数值，使用基于 Bruls, Huizing, van Wijk 的 "Squarified Treemaps" 算法
+    计算指定几何形状的矩形树图布局。请参阅 README 以了解示例用法。
+
+    参数
+    ----------
+    sizes : 类似列表的数值序列
+        计算矩形树图所需的一组数值。`sizes` 必须是正值，按降序排序，
+        并且应归一化为总面积（即，`dx * dy == sum(sizes)`）
+    x, y : 数值
+        “原点”的坐标。
+    dx, dy : 数值
+        矩形树图的完整宽度（`dx`）和高度（`dy`）。
+
+    返回
+    -------
+    list[dict]
+        返回的列表中的每个字典代表矩形树图中的单个矩形。顺序对应于输入顺序。
+    """
+    
+    sizes = list(map(float, sizes))  # 将 sizes 转换为浮点数列表
+
+    if len(sizes) == 0:  # 如果 sizes 为空
         return []
 
-    if len(sizes) == 1:
+    if len(sizes) == 1:  # 如果 sizes 只有一个元素
         return layout(sizes, x, y, dx, dy)
 
-    # figure out where 'split' should be
+    # 确定 'split' 应该在哪里
     i = 1
-    while i < len(sizes) and worst_ratio(sizes[:i], x, y, dx, dy) >= worst_ratio(
-        sizes[: (i + 1)], x, y, dx, dy
-    ):
-        i += 1
-    current = sizes[:i]
-    remaining = sizes[i:]
+    while i < len(sizes) and worst_ratio(sizes[:i], x, y, dx, dy) >= worst_ratio(sizes[: (i + 1)], x, y, dx, dy):
+        # 刚开始，x,y = 0,0 
+        # worst_ratio(sizes[:i], x, y, dx, dy) 为 sizes[:i] 的最大长宽比
+        # 如果多个子矩阵，长宽比更大，则放弃
+        i += 1  # 寻找最佳分割点
+        
+    current = sizes[:i]  # 当前分割点之前的部分
+    remaining = sizes[i:]  # 当前分割点之后的部分
 
+    # 计算剩余部分的坐标和尺寸
     (leftover_x, leftover_y, leftover_dx, leftover_dy) = leftover(current, x, y, dx, dy)
+    
+    # 对当前部分进行布局，并递归处理剩余部分
     return layout(current, x, y, dx, dy) + squarify(
         remaining, leftover_x, leftover_y, leftover_dx, leftover_dy
     )
+
     
 
 
@@ -194,21 +219,21 @@ padded_squarify函数的输入参数和输出格式与squarify函数相同。
 
 
 def normalize_sizes(sizes, dx, dy):
-    """Normalize list of values.
+    """归一化数值列表。
 
-    Normalizes a list of numeric values so that `sum(sizes) == dx * dy`.
+    将一系列数值归一化，使得 `sum(sizes) == dx * dy`。
 
-    Parameters
+    参数
     ----------
-    sizes : list-like of numeric values
-        Input list of numeric values to normalize.
-    dx, dy : numeric
-        The dimensions of the full rectangle to normalize total values to.
+    sizes : 类似列表的数值序列
+        输入要归一化的数值列表。
+    dx, dy : 数值
+        用于归一化总值的完整矩形的尺寸。
 
-    Returns
+    返回
     -------
     list[numeric]
-        The normalized values.
+        归一化后的数值。
     """
     total_size = sum(sizes)
     total_area = dx * dy
@@ -292,7 +317,7 @@ def plot(
     if len(kwargs) > 0:
         bar_kwargs.update(kwargs)
 
-    normed = normalize_sizes(sizes, norm_x, norm_y)
+    normed = normalize_sizes(sizes, norm_x, norm_y) # 计算对应矩形的面积
 
     if pad:
         rects = padded_squarify(normed, 0, 0, norm_x, norm_y,pad)
